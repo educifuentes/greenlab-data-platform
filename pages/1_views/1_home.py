1dbd1b6e146836d4f81cf0d2a28af033b70e89db
educifuentes <edu.cifuentes@gmail.com>
2026-03-18 10:42:35 -0300

    fix

--- pages/1_views/1_home.py
+++ pages/1_views/1_home.py
@@ -4,17 +4,3 @@ from helpers.ui_components.icons import ICONS, render_icon
 
 
 st.title(f"{render_icon('logo')} Greenlab Data Platform")
-
-st.header("Features")
-
-st.markdown("- Catalogo de datos")
-st.markdown("- Replicabilidad en trasnformciones en el data pipleline")
-st.markdown("- Tranparencia gracias a Control de versiones (uso de git)")
-st.markdown("- Podrorso y felxible: python para automatiuzar casi cualquier escensario")
-st.markdown("- UI amigable y facil de usar")
-
-
-
-st.subheader("Ejemplos")
-
-st.page_link("pages/1_views/2_contaminantes.py", icon=":material/co2:")
