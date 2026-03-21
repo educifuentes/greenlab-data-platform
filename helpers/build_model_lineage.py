02ae6c09783645f7717bc3a0a12184a534983f7e
educifuentes <edu.cifuentes@gmail.com>
2026-03-20 22:44:46 -0300

    fix - build_model_lineage

--- helpers/build_model_lineage.py
+++ helpers/build_model_lineage.py
@@ -27,9 +27,26 @@ def build_model_lineage():
     if model_name.startswith('_'):
         model_name = model_name[1:]
         
-    # 3. Parse the caller file to find imports from 'models'
     models_dict = {}
-    
+
+    # 3. Concatenate lineage from previous dataframes in the caller's local variables
+    try:
+        if len(stack) > 1:
+            caller_frame = stack[1].frame
+            for var_name, var_val in list(caller_frame.f_locals.items()):
+                if hasattr(var_val, 'attrs') and isinstance(var_val.attrs, dict):
+                    prev_models = var_val.attrs.get("models", {})
+                    if isinstance(prev_models, dict):
+                        for prev_layer, prev_list in prev_models.items():
+                            if prev_layer not in models_dict:
+                                models_dict[prev_layer] = []
+                            for m_name in prev_list:
+                                if m_name not in models_dict[prev_layer]:
+                                    models_dict[prev_layer].append(m_name)
+    except Exception:
+        pass
+
+    # 4. Parse the caller file to find direct imports from 'models'
     try:
         with open(caller_filename, 'r', encoding='utf-8') as f:
             tree = ast.parse(f.read(), filename=caller_filename)
@@ -56,6 +73,22 @@ def build_model_lineage():
                             models_dict[layer] = []
                         if src not in models_dict[layer]:
                             models_dict[layer].append(src)
+            elif isinstance(node, ast.Call):
+                if isinstance(node.func, ast.Name) and node.func.id == 'load_source_dataframe':
+                    if node.args:
+                        arg0 = node.args[0]
+                        if isinstance(arg0, ast.Constant) and isinstance(arg0.value, str):
+                            src_name = arg0.value
+                        elif hasattr(ast, 'Str') and isinstance(arg0, getattr(ast, 'Str')):
+                            src_name = arg0.s
+                        else:
+                            continue
+                            
+                        layer = 'source'
+                        if layer not in models_dict:
+                            models_dict[layer] = []
+                        if src_name not in models_dict[layer]:
+                            models_dict[layer].append(src_name)
     except Exception:
         pass # Fail gracefully if file cannot be parsed
         
